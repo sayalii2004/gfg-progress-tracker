0775a4ca3a09256429fcf0e22ae7d9d200b488d3
Sayali Shete <180266581+sayalii2004@users.noreply.github.com>
2026-02-28 10:29:11 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Anagram/anagram.java
+++ Difficulty: Easy/Anagram/anagram.java
@@ -1,17 +1,13 @@
 class Solution {
     public static boolean areAnagrams(String s1, String s2) {
         // code here
-        if(s1.length()!=s2.length())
-        {
-            return false;
-        }
+        if (s1.length()!=s2.length()) return false;
         
-        char [] arr1=s1.toCharArray();
-        char [] arr2=s2.toCharArray();
+        char []sorted1=s1.toCharArray();
+        char []sorted2=s2.toCharArray();
         
-        Arrays.sort(arr1);
-        Arrays.sort(arr2);
-        
-        return Arrays.equals(arr1,arr2);
+        Arrays.sort(sorted1);
+        Arrays.sort(sorted2);
+        return Arrays.equals(sorted1,sorted2);
     }
 }
